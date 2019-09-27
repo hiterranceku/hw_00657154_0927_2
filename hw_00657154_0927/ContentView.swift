68c82bf8d9c7bf39b331f6e7283f0207ec661b69
andy <andy870622@gmail.com>
2019-09-27 17:01:39 +0800

    0927 new

--- hw_00657154_0927/ContentView.swift
+++ hw_00657154_0927/ContentView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+
+        ZStack {
+            Path { (path) in
+            path.move(to: CGPoint(x: 160, y: 40))
+            path.addLine(to: CGPoint(x: 100, y: 150))
+            path.addLine(to: CGPoint(x: 120, y: 150))
+            path.addLine(to: CGPoint(x: 70, y: 240))
+            path.addLine(to: CGPoint(x: 90, y: 240))
+            path.addLine(to: CGPoint(x: 20, y: 330))
+                
+            path.addLine(to: CGPoint(x: 290, y: 330))
+            path.addLine(to: CGPoint(x: 230, y: 240))
+            path.addLine(to: CGPoint(x: 250, y: 240))
+            path.addLine(to: CGPoint(x: 200, y: 150))
+            path.addLine(to: CGPoint(x: 220, y: 150))
+                path.closeSubpath()
+            }
+                .fill(Color(red:0, green:0.4, blue:0.3))
+            
+            Path(ellipseIn: CGRect(x: 100, y: 240, width: 30, height: 30))
+                .fill(Color(red:1, green:0, blue:0))
+            
+            Path(ellipseIn: CGRect(x: 170, y: 200, width: 30, height: 30))
+            .fill(Color(red:1, green:1, blue:0))
+            
+            Path(ellipseIn: CGRect(x: 200, y: 280, width: 30, height: 30))
+                .fill(Color(red:1, green:1, blue:0))
+            
+            Path { (path) in
+               path.move(to: CGPoint(x: 130, y: 330))
+               path.addLine(to: CGPoint(x: 130, y: 380))
+               path.addLine(to: CGPoint(x: 190, y: 380))
+               path.addLine(to: CGPoint(x: 190, y: 330))
+               path.closeSubpath()
+            }
+            .fill(Color(red:0.3, green:0.1, blue:0.1))
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
