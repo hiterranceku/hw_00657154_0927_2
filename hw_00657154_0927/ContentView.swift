152f3ff02aead7e4ed82d3fd5c53e3c7fcdc35b9
andy <andy870622@gmail.com>
2019-09-27 16:17:22 +0800

    Initial Commit

---
+++ hw_00657154_0927/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw_00657154_0927
+//
+//  Created by User15 on 2019/9/27.
+//  Copyright © 2019 557021. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
